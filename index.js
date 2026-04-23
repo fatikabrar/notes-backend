b05dec8666a0628e25514982caa14b100cf674e2
fatikabrar60-rgb <fatikabrar60@gmail.com>
2026-04-23 17:53:36 +0500

    (index.js)

--- index.js
+++ index.js
@@ -10,11 +10,6 @@ require("./db");
 app.use(express.json());
 
 
-app.use((err, req, res, next) => {
-  console.error("Unhandled error:", err);
-  res.status(500).json({ message: "Internal server error" });
-});
-
 
 app.use(cors());
 //root(/)

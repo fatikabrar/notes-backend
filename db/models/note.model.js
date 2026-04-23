381f486da8ce17109ad11ced8573951e0e1f472c
fatikabrar60-rgb <fatikabrar60@gmail.com>
2026-04-23 19:02:35 +0500

    Fix Mongoose model overwrite issue causing crash on Railway

--- db/models/note.model.js
+++ db/models/note.model.js
@@ -1,6 +1,11 @@
-const mongoose = require("mongoose"); 
+const mongoose = require("mongoose");
+
 const noteSchema = new mongoose.Schema({
   text: String,
   link: String,
 });
-module.exports = mongoose.model('Note', noteSchema);
+
+// جلوگیری از duplicate model crash
+const noteModel = mongoose.models.Note || mongoose.model("Note", noteSchema);
+
+module.exports = noteModel;

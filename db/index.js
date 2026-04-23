7291b27dbc514aeb734978c4fdcc46519196e9b9
fatikabrar60-rgb <fatikabrar60@gmail.com>
2026-04-23 17:26:07 +0500

    Remove deprecated mongoose options and fix MongoDB URI

--- db/index.js
+++ db/index.js
@@ -5,6 +5,6 @@ const { DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME } = proces
 
 const uri = `mongodb+srv://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_URL}/${DATABASE_NAME}?retryWrites=true&w=majority`;
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(uri)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error("MongoDB connection error:", err));

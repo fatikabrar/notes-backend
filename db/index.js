eea8135b069dca3601e24764c40b467e274885c6
fatikabrar60-rgb <fatikabrar60@gmail.com>
2026-04-23 14:34:23 +0500

    Fix MongoDB connection string and port handling

--- db/index.js
+++ db/index.js
@@ -1,12 +1,10 @@
-const mongoose = require("mongoose"); 
- require('dotenv').config();  
+const mongoose = require("mongoose");
+require('dotenv').config();
 
-const { DATABASE_URL, DATABASE_USER ,DATABASE_PASSWORD, DATABASE_NAME } = process.env;
+const { DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME } = process.env;
 
-mongoose.connect(`mongodb+srv://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_URL}/${DATABASE_NAME}?retryWrites=true&w=majority`);
+const uri = `mongodb+srv://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_URL}/${DATABASE_NAME}?retryWrites=true&w=majority`;
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function() {
-  console.log("Connected to MongoDB");
-}); 
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
